7346ef230150b3ec622872de4086f0b3c33f757b
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-02-11 23:23:16 +0530

    mystery correction

--- app/components/todaysMystery.tsx
+++ app/components/todaysMystery.tsx
@@ -9,9 +9,9 @@ export const TodaysMystery=({gutt}:{gutt:RosaryOfTheDay[]})=>{
     let data = gutt[0]
     return(
         <>
-            <div className=" bg-red-500 h-2 my-8" ></div>
+            <div className=" text-orange-600  h-2 my-8" ></div>
         <h2 className=" text-3xl mt-8">Aicho gutt: {data.title}</h2>
-        <div className=" bg-red-500 h-2 my-8" ></div>
+        <div className=" text-orange-600 h-2 my-8" ></div>
         <h3 className="mt-8 text-2xl ">Dis: {mapDayToKonkani[getDay().toLowerCase()]} / <small> {getDay()}</small></h3>
         {
             data.mysteries.map((item,index)=>(
@@ -19,7 +19,7 @@ export const TodaysMystery=({gutt}:{gutt:RosaryOfTheDay[]})=>{
                 <h4 key={item.title} className="text-3xl my-8">
                     {`${index+1}] ${item.title}`}
                 </h4>
-                <div className=" bg-red-500 h-2 my-8 w-1/2" ></div>
+                <div className=" text-orange-600  h-2 my-8 w-1/2" ></div>
                 </>
             ))
         }
