1fa188bc361bf81bb2ed9675d5070886dc6f7fe5
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-02-12 01:42:44 +0530

    all rosaries corrected

--- app/util/index.ts
+++ app/util/index.ts
@@ -1,30 +1,28 @@
-import fsPromises from 'fs/promises';
-import path from 'path'
+import fsPromises from "fs/promises";
+import path from "path";
 
-
-export const getRosaries =async () => {
+export const getRosaries = async () => {
   const filePath = path.join(process.cwd(), "app/data/rosary.json");
   const jsonData = await fsPromises.readFile(filePath, "utf-8");
   const data = JSON.parse(jsonData);
-  return data
+  return data;
 };
 
-export const getDay=()=>{
-    const today = new Date()
-    var options = { weekday: 'long', timeZone: 'Asia/Kolkata' }; 
-    const dayOfWeek = today.toLocaleDateString('en-IN',{
-        weekday:'long',
-        timeZone:'Asia/Kolkata'
-    })
-    return dayOfWeek
-}
+export const getDay = () => {
+  const today = new Date();
+  const dayOfWeek = today.toLocaleDateString("en-IN", {
+    weekday: "long",
+    timeZone: "Asia/Kolkata",
+  });
+  return dayOfWeek;
+};
 
-export const mapDayToKonkani:any = {
-    sunday: "Aitar",
-    monday: "Somar",
-    tuesday: "Mûnglar",
-    wednesday: "Bûdvar",
-    thursday: "Brestar",
-    friday: "Sûkrar",
-    saturday: "Shenvar"
+export const mapDayToKonkani: any = {
+  sunday: "Aitar",
+  monday: "Somar",
+  tuesday: "Mûnglar",
+  wednesday: "Bûdvar",
+  thursday: "Brestar",
+  friday: "Sûkrar",
+  saturday: "Shenvar",
 };
