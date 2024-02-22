4b171b98e46ac94a36626db5a48e5d20a870378a
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-02-22 21:04:54 +0530

    types fixed

--- app/allmysteries/page.tsx
+++ app/allmysteries/page.tsx
@@ -1,14 +1,10 @@
 import BackButton from "../components/backButton";
-import { Rosaries } from "../types";
-import { getRosaries } from "../util";
 
-async function getData() {
-  const data = await getRosaries();
+import data from '@/app/data/rosary.json'
 
-  return data;
-}
+
 export default async function AllRosaries() {
-  const data: Rosaries = await getData();
+
   return (
     <>
       <BackButton />
--- app/components/todaysMystery.tsx
+++ app/components/todaysMystery.tsx
@@ -1,25 +1,31 @@
 "use client"
+import { useEffect, useState } from "react";
 import { getDay, mapDayToKonkani } from "../util";
 import rosaries from '@/app/data/rosary.json'
+import { RosaryOfTheDay } from "../types";
 
 export const TodaysMystery = () => {
+  const [todaysGut,setTodatsGut] = useState<RosaryOfTheDay>()
+  
+  useEffect(()=>{
+    const day = getDay()
+    const data = rosaries.filter((item)=>item.day.includes(day.toLowerCase()))[0]
+    setTodatsGut(data)
+  },[])
+  
+  
 
-  const day = getDay()
-  
-  
-  const data = rosaries.filter((item)=>item.day.includes(day.toLowerCase()))[0]
-  console.log("rosaries",data);
   
   return (
     <>
       <div className=" bg-orange-600  h-2 my-8"></div>
-      <h2 className=" text-3xl mt-8">Aicho gutt: {data?.title}</h2>
+      <h2 className=" text-3xl mt-8">Aicho gutt: {todaysGut?.title}</h2>
       <div className=" bg-orange-600 h-2 my-8"></div>
       <h3 className="mt-8 text-2xl ">
         Dis: {mapDayToKonkani[getDay().toLowerCase()]} /{" "}
         <small> {getDay()}</small>
       </h3>
-      {data?.mysteries.map((item, index) => (
+      {todaysGut?.mysteries.map((item, index) => (
         <div key={item.title}>
           <h4 key={item.title} className="text-3xl my-8">
             {`${index + 1}] ${item.title}`}
